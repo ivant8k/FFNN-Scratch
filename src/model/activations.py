439b3601427600fc227acb301d9e72a93b238603
ivant8k <13523129@std.stei.itb.ac.id>
2026-03-06 01:52:07 +0700

    feat: loss function + cleaning activation.py

--- src/model/activations.py
+++ src/model/activations.py
@@ -7,41 +7,41 @@ import numpy as np
 class Activation:
 
     # Linear: Linear(x) = x
-    def linear(x):
+    def linear(self, x):
         return x
 
-    def linear_derivative(x):
+    def linear_derivative(self, x):
         return 1
 
     # ReLU: ReLU(x) = max(0,x)
-    def relu(x):
+    def relu(self, x):
         return np.maximum(0, x)
 
-    def relu_derivative(x):
+    def relu_derivative(self, x):
         return np.where(x > 0, 1, 0)
 
     # Sigmoid: \sigma(x) = 1/(1+e^-x)
-    def sigmoid(x):
+    def sigmoid(self, x):
         return 1/ (1 + np.exp(-x))
 
-    def sigmoid_derivative(x):
-        return Activation.sigmoid(x) * (1 - Activation.sigmoid(x))
+    def sigmoid_derivative(self, x):
+        return self.sigmoid(x) * (1 - self.sigmoid(x))
 
     # Hyperbolic Tangent (tanh): tanh(x)=(e^x - e^-x)/(e^x + e^x)
-    def tanh(x):
+    def tanh(self, x):
         return np.tanh(x)
 
-    def tanh_derivative(x):
+    def tanh_derivative(self, x):
         return (2 / np.exp(x) - np.exp(-x))**2
 
     # Softmax: For vector \overrightarrow(x) = (x_1, x_2, ..., x_n) \in \mathbb{R}^n
     # softmax(\overrightarrow(x))_i = (e^(x_i) / \sum_{j=1}{n} e^(x_j))
-    def softmax(x):
+    def softmax(self, x):
         exp_x = np.exp(x - np.max(x))
         return exp_x / np.sum(exp_x)
 
-    def softmax_derivative(x):
-        S = Activation.softmax(x)
+    def softmax_derivative(self, x):
+        S = self.softmax(x)
         S_vector = S.reshape(-1,1)
         diag = np.diagflat(S)
         jacobian = diag - np.dot(S_vector, S_vector.T)
@@ -50,49 +50,49 @@ class Activation:
 
 
     # (Bonus) Leaky ReLU: Leaky ReLU(x) = {x | x > 0} or {\alpha x | x <= 0}
-    def leaky_relu(x, alpha=0.01):
+    def leaky_relu(self, x, alpha=0.01):
         return np.where(x > 0, x, alpha * x)
 
-    def leaky_relu_derivative(x, alpha=0.01):
+    def leaky_relu_derivative(self, x, alpha=0.01):
         return np.where(x>0, 1, alpha)
 
     # (Bonus) SoftPlus: A(x) = log(1 + e^x)
-    def softplus(x):
+    def softplus(self, x):
         return np.log10(1 + np.exp(x))
 
-    def softplus_derivative(x):
+    def softplus_derivative(self, x):
         return np.exp(x)/(np.log(10) * (1 + np.exp(x)))
 
 
 
-test_val = 0.5
-test_arr = np.array([-1.0, 0.0, 1.0, 2.0])
-
-print(">>>Testing Fungsi Aktivasi\n")
-#print("1. Linear")
-#print(f"Linear({test_val}):", Activation.linear(test_val))
-#print(f"Linear Derivative({test_val}):", Activation.linear_derivative(test_val))
-#print()
-#print("2. ReLU")
-#print(f"ReLU({test_arr}):", Activation.relu(test_arr))
-#print(f"ReLu Derivative({test_arr}):", Activation.relu_derivative(test_arr))
-#print()
-#print("3. Sigmoid")
-#print("Sigmoid(0):", Activation.sigmoid(0))
-#print("Sigmoid Derivative(0):", Activation.sigmoid_derivative(0))
-#print()
-#print("4. Hyperbolic Tangent (tanh)")
-#print(f"Tanh({test_val}):", Activation.tanh(test_val))
-#print(f"Tanh Derivative({test_val}):", Activation.tanh_derivative(test_val))
-#print()
-print("5. Softmax")
-print(f"Softmax({test_arr}):", Activation.softmax(test_arr))
-print(f"Softmax({test_arr}):", Activation.softmax_derivative(test_arr))
-#print()
-#print("6. Leaky ReLU")
-#print(f"Leaky ReLU({test_arr}):", Activation.leaky_relu(test_arr))
-#print(f"Leaky ReLU Derivative({test_arr}):", Activation.leaky_relu_derivative(test_arr))
-#print()
-#print("7. Softplus")
-#print(f"Softplus({test_val}):", Activation.softplus(test_val))
-#print(f"Softplus Derivative({test_val}):", Activation.softplus_derivative(test_val))
+# test_val = 0.5
+# test_arr = np.array([-1.0, 0.0, 1.0, 2.0])
+# Activation = Activation()
+# print(">>>Testing Fungsi Aktivasi\n")
+# print("1. Linear")
+# print(f"Linear({test_val}):", Activation.linear(test_val))
+# print(f"Linear Derivative({test_val}):", Activation.linear_derivative(test_val))
+# print()
+# print("2. ReLU")
+# print(f"ReLU({test_arr}):", Activation.relu(test_arr))
+# print(f"ReLu Derivative({test_arr}):", Activation.relu_derivative(test_arr))
+# print()
+# print("3. Sigmoid")
+# print("Sigmoid(0):", Activation.sigmoid(0))
+# print("Sigmoid Derivative(0):", Activation.sigmoid_derivative(0))
+# print()
+# print("4. Hyperbolic Tangent (tanh)")
+# print(f"Tanh({test_val}):", Activation.tanh(test_val))
+# print(f"Tanh Derivative({test_val}):", Activation.tanh_derivative(test_val))
+# print()
+# print("5. Softmax")
+# print(f"Softmax({test_arr}):", Activation.softmax(test_arr))
+# print(f"Softmax({test_arr}):", Activation.softmax_derivative(test_arr))
+# print()
+# print("6. Leaky ReLU")
+# print(f"Leaky ReLU({test_arr}):", Activation.leaky_relu(test_arr))
+# print(f"Leaky ReLU Derivative({test_arr}):", Activation.leaky_relu_derivative(test_arr))
+# print()
+# print("7. Softplus")
+# print(f"Softplus({test_val}):", Activation.softplus(test_val))
+# print(f"Softplus Derivative({test_val}):", Activation.softplus_derivative(test_val))
--- src/model/loss.py
+++ src/model/loss.py
@@ -1,29 +1,52 @@
 # --------------------------------------------------------------
 # Loss Functions
 # --------------------------------------------------------------
+import numpy as np
 
 class Loss:
     # Mean Squared Error: MSE = 1/n * (\sum_{i=1}^{n}(y_{i}-\hat{y})^2
-    def mse(y_a, y_p):
+    def mse(self, y_a, y_p, epsilon=1e-15):
         return np.mean((y_a - y_p)**2)
     
-    def mse_derivative(y_a, y_p):
-        pass
+    def mse_derivative(self, y_a, y_p):
+        return -2 * (y_a - y_p) / y_p.shape[0]
 
     # Binary Cross-Entropy:
     # \mathcal{L}_{BCE} = -1/n * \sum_{i=1}^{n}(y_{i} log \hat{y} + (1-y_{i}) log (1-\hat{y}))
-    def binary_cross_entropy(y_a,y_p):
-        pass
+    def binary_cross_entropy(self, y_a,y_p, epsilon=1e-15):
+        y_p = np.clip(y_p, epsilon, 1 - epsilon)
+        return -np.mean(y_a * np.log(y_p) + (1-y_a) * np.log(1-y_p))
 
-    def binary_cross_entropy_derivative(y_a,y_p):
-        pass
+    def binary_cross_entropy_derivative(self, y_a,y_p, epsilon=1e-15):
+        y_p = np.clip(y_p, epsilon, 1-epsilon)
+        return -((y_p - y_a)/(y_p * (1-y_p))) / y_p.shape[0]
 
     # Categorical Cross-Entropy: 
     # \mathcal{L}_{CCE} = -1/n * \sum_{i=1}^{n} \sum_{j=1}^{C} (y_{ij} log \hat{y}_{ij})
-    def categorical_cross_entropy(y_a, y_p):
-        pass
+    def categorical_cross_entropy(self, y_a, y_p, epsilon=1e-15):
+        y_p = np.clip(y_p, epsilon, 1-epsilon)
+        return -np.mean(np.sum(y_a * np.log(y_p), axis = 1))
 
-    def categorical_cross_entropy_derivative(y_a, y_p):
-        pass
+    def categorical_cross_entropy_derivative(self, y_a, y_p, epsilon=1e-15):
+        y_p = np.clip(y_p, epsilon, 1-epsilon)
+        return -(y_a / (y_p * y_p.shape[0]))
 
-print("Testing Loss Functions")
+# y_actual_arr = np.array([[1, 0, 0], [0,1,0], [0,0,1]])
+# y_pred_arr = np.array([[0.8, 0.1, 0.1], [0.2, 0.8, 0.0], [0.3, 0.3, 0.4]])
+# print(">>> Testing Loss Functions")
+# loss = Loss()
+# print("Actual: \n", y_actual_arr)
+# print("Predict: \n", y_pred_arr)
+# print()
+# print("1. MSE")
+# print("MSE: ", loss.mse(y_actual_arr, y_pred_arr))
+# print("MSE Derivative: ", loss.mse_derivative(y_actual_arr, y_pred_arr))
+# print()
+# print("2. BCE")
+# print("BCE: ", loss.binary_cross_entropy(y_actual_arr, y_pred_arr))
+# print("BCE Derivative: ", loss.binary_cross_entropy_derivative(y_actual_arr, y_pred_arr))
+# print()
+# print("3. CCE")
+# print("CCE: ", loss.categorical_cross_entropy(y_actual_arr, y_pred_arr))
+# print("CCE Derivative: ", loss.categorical_cross_entropy_derivative(y_actual_arr, y_pred_arr))
+# print()
